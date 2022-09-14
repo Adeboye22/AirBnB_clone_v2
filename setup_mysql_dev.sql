8f452085739644ba4c05aa40da51140072f0d9ba
Oluwadamilola <OyeniyaOluwadamilola@outlook.com>
2022-09-14 01:50:19 -0700

    MySQL setup development

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,6 @@
+-- prepares MySQL server for project
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `peforrmance_schema`.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
