4b390e1dbcf9ed99a7524fe56575446559f7e3e1
Oluwadamilola <OyeniyaOluwadamilola@outlook.com>
2022-10-16 18:39:08 -0700

    initial commit

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,62 +1,33 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
+'''A simple Flask web application.
+'''
+from flask import Flask, render_template
 
-from flask import Flask
-from flask import render_template
+from models import storage
+from models.state import State
+
 
 app = Flask(__name__)
+'''The Flask application instance.'''
+app.url_map.strict_slashes = False
 
 
-@app.route("/", strict_slashes=False)
-def index():
-    return "Hello HBNB!"
-
-
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    return "HBNB"
-
-
-@app.route("/c/<text>", strict_slashes=False)
-def C_is_fun(text):
-    return "C " + text.replace("_", " ")
-
-
-@app.route("/python", strict_slashes=False)
-def python():
-    return "Python is cool"
-
-
-@app.route("/python/<text>", strict_slashes=False)
-def Python_is_magic(text):
-    return "Python " + text.replace("_", " ")
-
-
-@app.route("/number/<int:n>", strict_slashes=False)
-def n_is_a_number(n):
-    return "{:d} is a number".format(n)
-
-
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    return render_template('5-number.html', num=n)
-
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def number_odd_or_even(n):
-    return render_template('6-number_odd_or_even.html', num=n)
-
-
-@app.route('/states_list', strict_slashes=False)
+@app.route('/states_list')
 def states_list():
-    return render_template('7-states_list.html',
-                           states=storage.all("State"))
+    '''The states_list page.'''
+    all_states = list(storage.all(State).values())
+    all_states.sort(key=lambda x: x.name)
+    ctxt = {
+        'states': all_states
+    }
+    return render_template('7-states_list.html', **ctxt)
 
 
 @app.teardown_appcontext
-def teardown(err):
+def flask_teardown(exc):
+    '''The Flask app/request context end event listener.'''
     storage.close()
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
