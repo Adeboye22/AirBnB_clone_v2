debf209c37b2c775fec07faa285ecf1dd4af6e27
Oluwadamilola Oyeniya <oyeniyaoluwadamilola@outlook.com>
2022-10-19 22:09:17 +0100

    initial commit

--- models/state.py
+++ models/state.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import os
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-
-from models.base_model import BaseModel, Base
+from os import getenv
+import models
+from models.base_model import Base
+from models.base_model import BaseModel
 from models.city import City
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = 'states'
-    name = Column(
-        String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship(
-            'City',
-            cascade='all, delete, delete-orphan',
-            backref='state'
-        )
-    else:
+    """ State class for a MySQL database"""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
+    
+    def __init__(self, *args, **kwargs):
+        """Initialises state"""
+        super().__init__(*args, **kwargs)
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
-            """Returns the cities in this State"""
-            from models import storage
-            cities_in_state = []
-            for value in storage.all(City).values():
-                if value.state_id == self.id:
-                    cities_in_state.append(value)
-            return cities_in_state
+            """returns list of City instances with state_id equal to State.id"""
+            city_instance_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_instance_list.append(city)
+            return city_instance_list
